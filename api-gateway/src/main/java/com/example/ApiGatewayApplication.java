2f4dd1233c2162ba0b0116b0818a4188fcc97f2a
ashoksurya.p@gmail.com <Ganesh@123>
2019-02-24 10:48:24 +0530

    Upgraded to spring boot & spring cloud latest versions

--- api-gateway/src/main/java/com/example/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/example/ApiGatewayApplication.java
@@ -7,6 +7,7 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableZuulProxy
+@EnableDiscoveryClient
 public class ApiGatewayApplication {
 
     public static void main(String[] args) {
